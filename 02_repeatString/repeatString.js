a93c92b9325c9ab462e92ac04eb882c13e79a4df
ThomassMathisen <thomasmathisen@hotmail.com>
2022-08-23 20:52:26 +0200

    Done first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    if (number < 0) {
+        return "ERROR"
+    }
+        return text.repeat(number)
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
