d50e516fc0785ed979df6552455798ba63257632
ThomassMathisen <thomasmathisen@hotmail.com>
2022-08-24 16:00:41 +0200

    Finished exercises for Fundamentals Part 4 of TOP

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  const c = (f - 32) * (5/9);
+  const rounded = Math.round(c * 10) /10
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c1) {
+  f1 = (c1 * 9/5) + 32;
+  const rounded1 = Math.round(f1 * 10) /10
+  return rounded1;
 };
 
 // Do not edit below this line
@@ -11,3 +15,4 @@ module.exports = {
   ftoc,
   ctof
 };
+
